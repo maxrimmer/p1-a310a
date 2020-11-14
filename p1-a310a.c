f305844e27e07e5cba113549ea31657c9b955f9b
Max Rimmer Larsen <maxrimmer@me.com>
2020-11-14 09:44:43 +0100

    Initial commit of p1-a310a.c

---
+++ p1-a310a.c
@@ -0,0 +1,23 @@
+/*
+P1 Projekt
+Gruppe: A310a
+Projektperiode: Efteraarssemester 2020
+*/
+
+/* Includes */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+/* Prototypes */
+
+
+
+/* Main */
+int main(int argc, char const *argv[]) {
+
+  printf("P1 Projekt test\n");
+
+  return EXIT_SUCCESS;
+}
