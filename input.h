e63bcc61d15863bee4e48797fefef2eddaec5767
DunkeyDunk <malthe23@live.dk>
2020-12-01 10:14:00 +0100

    Created a validation function for the input
    This function returns true if the input is between the upper and lower boundry

--- input.h
+++ input.h
@@ -1,3 +1,5 @@
+/*This function returns ture if the input is between the upper and lower boundry*/
+int inputVarification(int input, int lower_boundry, int upper_boundry);
 
 struct area read_input(void) {
   struct area area;
@@ -19,27 +21,31 @@ struct area read_input(void) {
   do {
     printf("Indtast venligst MFO arealets jords tunghed. (1-10, heltal):\n");
     scanf(" %d", &area.heaviness);
-  } while(area.heaviness < 1 || area.heaviness > 10);
+  } while(!inputVarification(area.heaviness, 1, 10));
 
   do {
     printf("Indtast venligst MFO arealets jords lysforhold. (1-10, heltal):\n");
     scanf(" %d", &area.light);
-  } while(area.light < 1 || area.light > 10);
+  } while(!inputVarification(area.light, 1, 10));
 
   do {
     printf("Indtast venligst MFO arealets jords pH-vaerdi. (0-14, heltal):\n");
     scanf(" %d", &area.pH);
-  } while(area.pH < 0 || area.pH > 14);
+  } while(!inputVarification(area.pH, 0, 14));
 
   do {
     printf("Indtast venligst MFO arealets jords naeringsstofsindhold. (1-10, heltal):\n");
     scanf(" %d", &area.nutrient);
-  } while(area.nutrient < 1 || area.nutrient > 10);
+  } while(!inputVarification(area.nutrient, 1, 10));
 
   do {
     printf("Indtast venligst MFO arealets jords fugtighed. (1-10, heltal):\n");
     scanf(" %d", &area.moistness);
-  } while(area.moistness < 1 || area.moistness > 10);
+  } while(!inputVarification(area.moistness, 1, 10));
 
   return area;
+}
+
+int inputVarification(int input, int lower_boundry, int upper_boundry){
+  return lower_boundry < input && input < upper_boundry;
 }
