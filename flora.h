e36d5aebb7ab4bac80e9e91e4b6f369b5f671817
AHougaard <74406435+AHougaard@users.noreply.github.com>
2020-12-01 13:00:41 +0100

    Update flora.h
    flora_matching added

--- flora.h
+++ flora.h
@@ -50,6 +50,46 @@ int hash(char *str) {
     return hash;
 }
 
+void flora_matching(struct flora *flora_array, struct area area, char **resultlist){
+  int a, count = 0;
+  for(a = 0; a < FLORA_HASH_ARRAY_SIZE; a++){
+    if(strcmp(flora_array[a].latinName, "") != 0){
+      if(is_match_flora(flora_array[a], area)){
+        resultlist[count] = flora_array->latinName;
+        count++;
+      }
+    }
+  }
+}
+
+int is_match_flora(struct flora flora_array, struct area area){
+  int a = 1;
+
+  if(flora_matching_checking(area.heaviness, flora_array->light))
+    a = 0;
+
+  if(flora_matching_checking(area.light, flora_array->light))
+    a = 0;
+
+  if(flora_matching_checking(area.moistness, flora_array.moistness))
+    a = 0;
+
+  if(flora_matching_checking(area.nutrient, flora_array.nutrient))
+    a = 0;
+
+  if(flora_matching_checking(area.pH, flora_array.pH))
+    a = 0;
+
+  return a;
+}
+
+int flora_matching_checking(int area, int flora_array){
+  if((area - flora_array) > 1 && (area - flora_array) < -1){
+    return 1;
+  }
+  return 0;
+}
+
 void printFloraTest() {
   printf("Flora header file working\n");
   int i;
