2d3903e08655266af04112cf43cbbc5b762eeebf
Max Rimmer Larsen <maxrimmer@me.com>
2020-12-08 10:31:00 +0100

    fixed inputVerification

--- input.h
+++ input.h
@@ -1,6 +1,6 @@
 #define HA_TO_M2_CONVERSIONRATIO 10000
 /*This function returns ture if the input is between the upper and lower boundry*/
-int inputVarification(int input, int lower_boundry, int upper_boundry);
+int inputVerification(int input, int lower_boundry, int upper_boundry);
 int ha_to_m2_converter(float ha);
 
 struct area read_input(void) {
@@ -26,32 +26,32 @@ struct area read_input(void) {
   do {
     printf("Indtast venligst MFO arealets jords tunghed. (1-10, heltal):\n");
     scanf(" %d", &area.heaviness);
-  } while(!inputVarification(area.heaviness, 1, 10));
+  } while(!inputVerification(area.heaviness, 1, 10));
 
   do {
     printf("Indtast venligst MFO arealets jords lysforhold. (1-10, heltal):\n");
     scanf(" %d", &area.light);
-  } while(!inputVarification(area.light, 1, 10));
+  } while(!inputVerification(area.light, 1, 10));
 
   do {
     printf("Indtast venligst MFO arealets jords pH-vaerdi. (0-14, heltal):\n");
     scanf(" %d", &area.pH);
-  } while(!inputVarification(area.pH, 0, 14));
+  } while(!inputVerification(area.pH, 0, 14));
 
   do {
     printf("Indtast venligst MFO arealets jords naeringsstofsindhold. (1-10, heltal):\n");
     scanf(" %d", &area.nutrient);
-  } while(!inputVarification(area.nutrient, 1, 10));
+  } while(!inputVerification(area.nutrient, 1, 10));
 
   do {
     printf("Indtast venligst MFO arealets jords fugtighed. (1-10, heltal):\n");
     scanf(" %d", &area.moistness);
-  } while(!inputVarification(area.moistness, 1, 10));
+  } while(!inputVerification(area.moistness, 1, 10));
 
   return area;
 }
 
-int inputVarification(int input, int lower_boundry, int upper_boundry){
+int inputVerification(int input, int lower_boundry, int upper_boundry){
   return lower_boundry <= input && input <= upper_boundry;
 }
 
