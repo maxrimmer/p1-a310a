a61b638a7e5843d19536756d77db2e1694553007
DunkeyDunk <40019807+DunkeyDunk@users.noreply.github.com>
2020-12-15 09:39:16 +0100

    comment fix

--- fauna.h
+++ fauna.h
@@ -18,11 +18,11 @@ void print_matched_flora(struct matched_flora *matched_flora);
 
 void fauna_database_and_matching(struct matched_flora *matched_flora, struct fauna *fauna) {
   int i;
-  struct fauna empty = {"", "", 0};
+  struct fauna emptyFauna = {"", "", 0};
 
   /* Empties fauna array */
   for (i = 0; i < HASH_ARRAY_SIZE; i++) {
-    fauna[i] = empty;
+    fauna[i] = emptyFauna;
   }
 
   read_fauna_database(fauna);
