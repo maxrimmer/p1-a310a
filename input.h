bfded5e177fd37c6970e7dab9d0c00002580e492
Max Rimmer Larsen <maxrimmer@me.com>
2021-01-16 13:13:28 +0100

    Changed get_input

--- input.h
+++ input.h
@@ -2,9 +2,9 @@
 /*This function returns ture if the input is between the upper and lower boundry*/
 int inputVerification(int input, int lower_boundry, int upper_boundry);
 int ha_to_m2_converter(float ha);
-void error_if_input_is_outside_range(int *checker);
+void error_if_input_is_outside_range(int checker);
 int get_input(int is_ellenberg_value, int lower_boundry, int upper_boundry);
-int ellenberg_value_condition (float input, int is_ellenberg_value, float lower_boundry, float upper_boundry);
+int value_verification (float input, int is_ellenberg_value, float lower_boundry, float upper_boundry);
 
 struct area read_input(void) {
   struct area area;
@@ -39,6 +39,38 @@ struct area read_input(void) {
 
   return area;
 }
+
+int get_input(int is_area_size, int lower_boundry, int upper_boundry) {
+  int checker = 0;
+  float input;
+  do {
+    error_if_input_is_outside_range(checker);
+    checker = 1;
+    scanf(" %f", &input);
+  } while(value_verification(input, is_area_size, lower_boundry, upper_boundry));
+  /*if it isn't a ellenberg value, then it needs to be converted from ha to m2*/
+  if (!is_area_size) {
+    input = ha_to_m2_converter(input);
+  }
+
+  return (int) input;
+}
+
+void error_if_input_is_outside_range(int checker) {
+  if (checker == 1) {
+    printf("Den indtastede vaerdi er ikke inden for det paakraevede interval.\n");
+    printf("\a");
+  }
+}
+
+int value_verification(float input, int is_ellenberg_value, float lower_boundry, float upper_boundry) {
+  if (is_ellenberg_value == 1) {
+    return !inputVerification(input, lower_boundry, upper_boundry);
+  }
+
+  return input <= 0.0;
+}
+
 /*Bool function that verifies if the input is between the boundries*/
 int inputVerification(int input, int lower_boundry, int upper_boundry) {
   return lower_boundry <= input && input <= upper_boundry;
@@ -46,37 +78,4 @@ int inputVerification(int input, int lower_boundry, int upper_boundry) {
 
 int ha_to_m2_converter(float ha) {
   return ha * HA_TO_M2_CONVERSIONRATIO;
-}
-
-int get_input(int is_ellenberg_value, int lower_boundry, int upper_boundry) {
-  int checker = 0;
-  float input;
-  do {
-    fflush(stdin);
-    error_if_input_is_outside_range(&checker);
-    scanf(" %f", &input);
-  } while(ellenberg_value_condition(input, is_ellenberg_value, lower_boundry, upper_boundry));
-  /*if it isn't a ellenberg value, then it needs to be converted from ha to m2*/
-  if (!is_ellenberg_value) {
-    input = ha_to_m2_converter(input);
-  }
-
-  return (int) input;
-}
-
-void error_if_input_is_outside_range(int *checker) {
-  if (*checker >= 1) {
-    printf("Den indtastede vaerdi er ikke inden for det paakraevede interval.\n");
-    printf("\a");
-  }
-
-  *checker += 1;
-}
-
-int ellenberg_value_condition(float input, int is_ellenberg_value, float lower_boundry, float upper_boundry) {
-  if (is_ellenberg_value == 1) {
-    return !inputVerification(input, lower_boundry, upper_boundry);
-  }
-
-  return input <= 0.0;
 }
